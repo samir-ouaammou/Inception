b6eec7db99386ba4a06be551c19175c551115235
samir-ouaammou <samirouaammou.4.4.200@gmail.com>
2025-09-08 15:21:07 +0100

    update all

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-service mariadb start 
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" 
-mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" 
-mysql -u root -e "FLUSH PRIVILEGES;" 
+mysqld_safe &
+db_pid=$!
 
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<STOP
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+STOP
 
-mysqld
+kill "$db_pid"
+wait "$db_pid" 2>/dev/null
+
+exec mysqld_safe
